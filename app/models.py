7c8fdfb947ff029d8cdc212f73a0528bfa9b6142
jaehoonkim.dev@gmail.com <jaehoonkim.dev@gmail.com>
2021-01-09 15:07:56 +0900

    fix: fix typo in models.py

--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ class CalendarEvent(models.Model):
     title = models.CharField(_('Title'), max_length=200)
     start = models.DateTimeField(_('Start'))
     end = models.DateTimeField(_('End'))
-    groundId = models.CharField(_('GroupId'), max_length=100, default=False)
+    groupId = models.CharField(_('GroupId'), max_length=100, default=False)
 
     def __str__(self):
         return self.title
